98b0c854e328c65243c6740ed3c311e57f605108
Stuart Olivera <stuart@stuartolivera.com>
2015-02-06 01:57:04 -0500

    Switched to basic html resume

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+ruby IO.read(File.expand_path("../.ruby-version", __FILE__)).chomp
+
+gem "haml"
+gem "compass"
---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: './public',
+          port: '4000',
+          host: '*'
+        }
+      }
+    },
+    clean: {
+      build: {
+        src: ['public/javascripts', 'public/stylesheets']
+      }
+    },
+    coffee: {
+      compile: {
+        options: {
+          bare: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/coffeescript',
+          src: ['**/*.coffee', '**/*.js'],
+          dest: 'public/javascripts',
+          ext: '.js'
+        }]
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          bundleExec: true,
+          sassDir: 'app/stylesheets',
+          cssDir: 'public/stylesheets',
+          environment: 'production'
+        }
+      }
+    },
+    haml: {
+      dist: {
+        options: {
+          bundleExec: true,
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/views',
+          src: ['**/*.haml'],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
+    watch: {
+      coffee: {
+        files: ['app/coffeescript/**/*.coffee'],
+        tasks: ['coffee'],
+        options: {
+          livereload: true,
+        }
+      },
+      styles: {
+        files: ['app/stylesheets/**/*.sass'],
+        tasks: ['compass'],
+        options: {
+          livereload: true,
+        }
+      },
+      haml: {
+        files: ['app/views/**/*.haml'],
+        tasks: ['haml'],
+        options: {
+          livereload: true,
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'public'
+      },
+      src: ['**']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-haml2html');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('build', ['clean', 'coffee', 'compass', 'haml']);
+  grunt.registerTask('default', ['build', 'connect', 'watch']);
+};
